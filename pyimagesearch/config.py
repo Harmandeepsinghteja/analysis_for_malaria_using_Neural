2ed29d4f8f455aa3389288e81966d3cd2bdaa2dd
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2024-11-24 12:01:35 -0700

    Path updated

--- pyimagesearch/config.py
+++ pyimagesearch/config.py
@@ -5,10 +5,12 @@ ORIG_INPUT_DATASET = "/mnt/c/large_datasets/archive/cell_images/"
 # initialize the base path to the *new* directory that will contain
 # our images after computing the training and testing split
 BASE_PATH = "analysis_for_malaria_using_Neural"
+
+
 # derive the training, validation, and testing directories
-TRAIN_PATH = os.path.sep.join([BASE_PATH, "training"])
-VAL_PATH = os.path.sep.join([BASE_PATH, "validation"])
-TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
+TRAIN_PATH = os.path.sep.join([ORIG_INPUT_DATASET, "training"])
+VAL_PATH = os.path.sep.join([ORIG_INPUT_DATASET, "validation"])
+TEST_PATH = os.path.sep.join([ORIG_INPUT_DATASET, "testing"])
 # define the amount of data that will be used training
 TRAIN_SPLIT = 0.8
 # the amount of validation data will be a percentage of the
